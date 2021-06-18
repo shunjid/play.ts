036923bd1f00cd9f398095faf616b8e23bc44155
shunjid <shunjid.rahman@gmail.com>
2021-06-18 11:40:37 +0600

    Added types

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,13 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var today = new Date();
+console.log(today.getMonth());
+var person = {
+    age: 20,
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = __importDefault(require("axios"));
-var url = "https://jsonplaceholder.typicode.com";
-var logTodo = function (todo) {
-    console.log(todo.id, todo.title, todo.completed);
-};
-axios_1.default.get(url + "/todos/1").then(function (response) {
-    var todo = response.data;
-    logTodo(todo);
-});
+var Color = /** @class */ (function () {
+    function Color() {
+    }
+    return Color;
+}());
+var red = new Color();
 //# sourceMappingURL=app.js.map
