dc92a1ca4497d2fff417c153edaf52bf4880b453
shunjid <shunjid.rahman@gmail.com>
2021-06-20 12:18:43 +0600

    Added tuples

---
+++ libs/tuples.ts
@@ -0,0 +1,7 @@
+// color, carbonated, sugar
+// type aliases
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ["brown", true, 40];
+const sprite: Drink = ["clear", true, 40];
+const tea: Drink = ["red", false, 0];
